da656ac5f8cf97d25644ea7cf2c2fa6cd181191b
Abhishek <108579783+Abhishek6543jh@users.noreply.github.com>
2023-09-21 22:14:21 +0530

    h

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-
+import instaloader
 app =FastAPI()
 
 @app.get('/')
 async def root():
+    L = instaloader.Instaloader()
+    profile = instaloader.Profile.from_username(L.context, "ravitechseries")
+    followeers = profile.followers
     return {
-        'hello':'hlo'
+        "followers":followeers
     }
