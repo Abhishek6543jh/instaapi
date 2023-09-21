c73380f7d1e3c9087e9bc85a65f0b978fa5c8168
Abhishek <108579783+Abhishek6543jh@users.noreply.github.com>
2023-09-21 23:10:59 +0530

    h

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ app =FastAPI()
 @app.get('/')
 async def root():
     L = instaloader.Instaloader()
+    #L.login("hlopy38", "hlo@hlo20")
+    #L.load_session_from_file() 
     profile = instaloader.Profile.from_username(L.context, "ravitechseries")
     followeers = profile.followers
     return {
